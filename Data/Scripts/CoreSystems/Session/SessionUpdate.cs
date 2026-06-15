e5573123be9dbcad48b252ccc8504a4c90c66d77
Aristeas <jnicktwentyfour@gmail.com>
2026-06-15 12:35:45 -0500

    Fix compilation error in latest SE update

--- Data/Scripts/CoreSystems/Session/SessionUpdate.cs
+++ Data/Scripts/CoreSystems/Session/SessionUpdate.cs
@@ -59,7 +59,7 @@ namespace CoreSystems
 
                 ai.CheckProjectiles = Tick - ai.NewProjectileTick <= 1;
 
-                if (ai.AiType == Ai.AiTypes.Grid && (ai.UpdatePowerSources || !ai.HadPower && ai.GridEntity.IsPowered || ai.HasPower && !ai.GridEntity.IsPowered || Tick10))
+                if (ai.AiType == Ai.AiTypes.Grid && (ai.UpdatePowerSources || !ai.HadPower && ai.GridEntity.IsPowerSwitchOn || ai.HasPower && !ai.GridEntity.IsPowerSwitchOn || Tick10))
                     ai.UpdateGridPower();
 
                 var enforcement = Settings.Enforcement;
